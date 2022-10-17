5b3b0de1a67465e3355255c462750b5e15a29ca4
Vinay Rao <vrao8912@gmail.com>
2022-10-17 16:50:42 +0200

    changed the app.js to include basic button funtionality

--- react-todolist-v1/src/App.js
+++ react-todolist-v1/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>ToDo App</h1>
+      <TodoForm />
     </div>
   );
 }
